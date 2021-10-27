bea4732b24e70da2b3827706c95030f4d8687e73
zzqcraft <zzqcraft@gmail.com>
2021-10-26 22:22:41 -0700

    Create 867.py

---
+++ 867.py
@@ -0,0 +1,11 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        m = len(matrix)
+        n = len(matrix[0])
+        result = [[0 for i in range(m)] for j in range(n)]
+        
+        for i in range(m):
+            for j in range(n):
+                result[j][i] = matrix[i][j]
+                
+        return result
